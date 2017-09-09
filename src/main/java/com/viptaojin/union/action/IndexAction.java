e08a437ea8ddfbe5eead8df2f8fab91eb40deb96
pangjian <pangj1990@163.com>
2017-09-09 17:24:13 +0800

    fix

--- src/main/java/com/viptaojin/union/action/IndexAction.java
+++ src/main/java/com/viptaojin/union/action/IndexAction.java
@@ -1,8 +1,10 @@
 package com.viptaojin.union.action;
 
+import com.viptaojin.union.common.data.ResultData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,5 +18,12 @@ public class IndexAction extends BaseAction{
     public String execute(HttpSession session, Model model){
         System.out.println("欢迎使用vip淘金网");
         return "index";
+    }
+
+    @RequestMapping("/applet")
+    @ResponseBody
+    public ResultData wxApplet(){
+        System.out.println("请求成功");
+        return toSuccessResultData("返回成功");
     }
 }
